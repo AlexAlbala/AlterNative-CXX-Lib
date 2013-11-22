cc7abc868fe101a4260bae8efc5cc6038fdb8f64
AlexAlbala <alexalbala88@gmail.com>
2013-11-22 09:41:36 +0100

    *** Fixed library bugs in linux

--- src/System/support/events.h
+++ src/System/support/events.h
@@ -8,4 +8,4 @@
 #define EVENT_INIT(del, name)				del* __internal__del = null;											\
 											name = __Internal__::Event::CreateEvent(__internal__del)
 
-#define EVENT_FIRE(eventName,...)			eventName.fireEvent(##__VA_ARGS__ )
+#define EVENT_FIRE(eventName,...)			eventName.fireEvent(__VA_ARGS__ )

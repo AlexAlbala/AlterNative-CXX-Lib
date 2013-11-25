c159ee815c226d8f6e57cbb91b914d4fdadf0b9f
Gerard <gerard@gsole.cat>
2013-11-25 20:00:51 +0100

    *** File::WriteAllText and File::ReadAllText

--- src/System/IO/File.cpp
+++ src/System/IO/File.cpp
@@ -1,6 +1,7 @@
 #include "File.h"
 #include "FileMode.h"
 #include "FileAccess.h"
+
 namespace System{
 	namespace IO{
 		bool File::Exists(String* path)
@@ -26,6 +27,54 @@ namespace System{
 			}
 			else
 				throw std::exception();//TODO: FILENOTFOUNDEXCEPTION
+		}
+
+		String* File::ReadAllText(String* path)
+		{
+			if (path == null)
+			{
+				throw new ArgumentNullException(new String("path"));
+			}
+			if (path->Length == 0)
+			{
+				// While we don't have the Environment class
+				//throw new ArgumentException(Environment.GetResourceString("Argument_EmptyPath"));
+				throw new ArgumentException(new String("Argument_EmptyPath"));
+			}
+			/*return File.InternalReadAllText(path, Encoding.UTF8, true);*/
+			return File::InternalReadAllText(path,  true);
+		}
+
+		void File::WriteAllText(String* path, String* contents)
+		{
+			if (path == null)
+			{
+				throw new ArgumentNullException(new String("path"));
+			}
+			if (path->Length == 0)
+			{
+				// While we don't have the Environment class
+				//throw new ArgumentException(Environment.GetResourceString("Argument_EmptyPath"));
+				throw new ArgumentException(new String("Argument_EmptyPath"));
+			}
+			File::InternalWriteAllText(path, contents, true);
+		}
+
+		/*String* InternalReadAllText(String* path, Encoding encoding, bool checkHost)*/
+		String* File::InternalReadAllText(String* path, bool checkHost)
+		{
+			String* result;
+			StreamReader* streamReader = new StreamReader(path);
+			result = streamReader->ReadToEnd();
+			return result;
+		}
+
+		void File::InternalWriteAllText(String* path, String* contents, bool checkHost)
+		{
+			StreamWriter* streamWriter = new StreamWriter(path);
+			streamWriter->Write(contents);
+			streamWriter->Flush();
+			streamWriter->Close();
 		}
 	}
 }
--- src/System/IO/File.h
+++ src/System/IO/File.h
@@ -5,15 +5,23 @@
 #include "FileStream.h"
 #include <fstream>
 #include <exception>
+#include "../Text/Encoding.h"
+#include "../exceptions/systemException/argumentException/ArgumentNullException.h"
 
 namespace System{
 	namespace IO{
 		class File : public virtual gc_cleanup{
+		private:
+			static String* InternalReadAllText(String* path, bool checkHost);
+			static void InternalWriteAllText(String* path, String* contents, bool checkHost);
+
 		public:
 			static bool Exists(String* path);
 			static void Delete(String* path);
 			static FileStream* OpenRead(String* path);
 			static FileStream* Create(String* path);
+			static String* ReadAllText(String* path);
+			static void WriteAllText(String* path, String* contents);
 		};
 	}
 }

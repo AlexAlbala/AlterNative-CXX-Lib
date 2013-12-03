8bc22adefa50061c1cf108431ce67325c084ffec
Gerard <gerard@gsole.cat>
2013-12-03 13:55:33 +0100

    *** DateTime::ToString()

--- src/System/DateTime.cpp
+++ src/System/DateTime.cpp
@@ -8,7 +8,6 @@ namespace System{
 
 	DateTime::DateTime()
 	{
-		TicksPerMillisecond = time_duration::ticks_per_second()/1000;
 	}
 
 	DateTime::DateTime(long long ticks)
@@ -18,14 +17,17 @@ namespace System{
 			//throw new ArgumentOutOfRangeException("ticks", "ArgumentOutOfRange_DateTimeBadTicks");
 		}
 		this->DateData = ticks;
-		TicksPerMillisecond = time_duration::ticks_per_second()/1000;
+		this->TicksPerMillisecond = time_duration::ticks_per_second()/1000;
 	}
 
 	DateTime::DateTime(int year, int month, int day)
 	{
-		ptime time_t_epoch(date(year,month,day));
-		this->DateData = static_cast<unsigned long>(time_t_epoch.time_of_day().ticks());
-		std::cout << this->DateData << std::endl;
+		ptime time_t_epoch(date(1970,1,1));
+		ptime d(date(year,month,day));
+		time_duration diff = d - time_t_epoch;
+		//DateTime(diff.ticks());
+		this->DateData=diff.ticks();
+		this->TicksPerMillisecond = time_duration::ticks_per_second()/1000;
 	}
 
 	DateTime* DateTime::GetNow()
@@ -46,18 +48,21 @@ namespace System{
 
 	String* DateTime::ToString()
 	{
-		static std::locale loc(std::wcout.getloc(), new wtime_facet(L"%d/%m/%Y %H:%M:%S"));
-		std::basic_stringstream<> wss;
-		wss.imbue(loc);
-		
+		/*static std::locale loc(std::wcout.getloc(), new wtime_facet(L"%d/%m/%Y %H:%M:%S"));*/
+
 		ptime epoch(date(1970,1,1));
 		time_duration duration = milliseconds(this->DateData/this->TicksPerMillisecond);
 		ptime t = epoch + duration;
 
-		wss << t;
-
-		//const char* a = wss.str();
-		std::wcout << wss.str() << std::endl;
-		return new String("");
+		std::string s;
+		std::ostringstream datetime_ss;
+		::boost::posix_time::time_facet * p_time_output = new ::boost::posix_time::time_facet;
+		std::locale special_locale (std::locale(""), p_time_output);
+		// special_locale takes ownership of the p_time_output facet
+		datetime_ss.imbue (special_locale);
+		(*p_time_output).format("%x %X"); // date time
+		datetime_ss << t;
+		s = datetime_ss.str().c_str();
+		return new String(s.data());
 	}
 }

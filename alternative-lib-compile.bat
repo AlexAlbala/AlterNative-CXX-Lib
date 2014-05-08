74170b4035cb85583d5e87006d15c6d643b9b527
AlexAlbala <alexalbala88@gmail.com>
2014-05-08 17:46:43 +0200

    +++ added script to compile alternative library in windows (linux script is coming...)

---
+++ alternative-lib-compile.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+set ALTERNATIVE_LIB_BIN=%ALTERNATIVE_CPP_LIB_PATH%\build\bin
+
+if not exist %ALTERNATIVE_LIB_BIN% mkdir %ALTERNATIVE_LIB_BIN%
+cd %ALTERNATIVE_LIB_BIN%
+del CMakeCache.txt
+cmake -G "Visual Studio 11" ..
+
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuildPath=%%D
+cd %msbuildPath%
+msbuild.exe %ALTERNATIVE_LIB_BIN%\AlterNativeLib.sln /p:PlatformToolset=v120_CTP_Nov2012
+
+::TODO -> How to fix these ugly paths ?
+cd %ALTERNATIVE_LIB_BIN%
+copy src\public\gc\Debug\gc-lib.lib ..\libfiles
+copy src\Debug\System.lib ..\libfiles
+cd %ALTERNATIVE_CPP_LIB_PATH%
